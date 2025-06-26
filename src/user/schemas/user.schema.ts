107cf459d89701e5aa45752fdf960412499ac9bc
leRangel18 <leticiacamargomarmo706@gmail.com>
2025-06-26 08:41:08 -0300

    feat: migra entidades de usuário para schemas MongoDB

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ type: [String], default: [] })
+  expenses: string[];
+
+  @Prop({ type: [String], default: [] })
+  incomes: string[];
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+
+  @Prop()
+  lastLogin: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User); 
